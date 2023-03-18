d337594ef9dd3f2e08f0a01b8b902f8427748eab
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-03-18 09:55:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
+
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({"activity":"Sit in the dark and listen to your favorite music with no distractions","type":"relaxation","participants":1,"price":0,"link":"","key":"9908721","accessibility":1})
+  const getData = () => {
+      fetch('http://www.boredapi.com/api/activity/')
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((err) => console.log(err))
+  }
+  useEffect(() => {
+
+    setInterval(() => {
+      getData();
+    }, 3000);
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>{data.activity}</p>
+        <a className="App-link" rel="noopener noreferrer">
+          {data?.type?.charAt(0).toUpperCase()+data?.type?.slice(1)}
         </a>
       </header>
     </div>
